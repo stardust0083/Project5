bd6aefa0529fe1838c64a1a834f1029d60cd7967
stardust0083 <hihi.love.good@163.com>
2021-11-21 08:12:09 +0800

    1

--- Source/Project5/Project5.Build.cs
+++ Source/Project5/Project5.Build.cs
@@ -9,6 +9,6 @@ public class Project5 : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[]
-			{"Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "Slate"});
+			{"Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "Slate","UMG"});
 	}
 }
